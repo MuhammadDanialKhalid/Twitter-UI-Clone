6a7c53205e24cfaa674de35e864aa41378134e6c
Danial Khalid <93424381+MuhammadDanialKhalid@users.noreply.github.com>
2024-11-27 00:01:27 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*html"],
+  theme: {
+    extend: {
+      width:{
+        'custom':"38px",
+      },
+      height:{
+        'custom':'34px'
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
